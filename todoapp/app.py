64f41cb191c4cdb87970973485f3cb1d99c61780
Ahmed Radi <ahmedradi743@gmail.com>
2020-09-29 14:09:51 +0200

    Add form
    Add form make it take input from the user and put it in the database then return it to the index page

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,21 +1,30 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://postgres:1234@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:1234@localhost:5432/todoapp'
 
-db  = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    __tablename__ =    'todos'
-    id = db.Column(db.Integer , primary_key=True)
-    description = db.Column(db.String() , nullable = False)
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f'<ToDo {self.id} {self.descreption} >'
+        return f'<ToDo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 @app.route("/")
 def index():
